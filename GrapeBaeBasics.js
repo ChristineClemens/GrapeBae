33eade2f8ad8aaf46d105dc0d8b297998ddd36f7
Christine Clemens <christineclemens.tesol@gmail.com>
2020-06-23 13:52:43 -0400

    Coded prompt statements and functionality.

--- GrapeBaeBasics.js
+++ GrapeBaeBasics.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
+
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const { start } = require('repl');
 
 // Database Class
 class Database {
@@ -36,3 +39,72 @@ const db = new Database({
     database: process.env.DB_NAME,
     insecureAuth : true
 });
+
+
+async function initialPrompt() {
+    const initialize = await inquirer.prompt (
+    {
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["Post an item", "Bid on an item"],
+        name: "action"
+    })
+    return initialize;
+}
+
+async function postItem() {
+    const postedItem = await inquirer.prompt ([
+    {
+        type: "input",
+        message: "What item would you like to post?",
+        name: "post"
+    },
+    {
+        type: "input",
+        message: "Please enter a brief description of the item:",
+        name: "description"
+    }])
+    console.log("Your item has been successfully posted!");
+    return postedItem;
+}
+
+async function bidItem() {
+    const submittedBid = await inquirer.prompt ([
+    {
+        type: "list",
+        message: "Which item would you like to bid on?",
+        choices: [""],
+        name: "items"
+    },
+    {
+        type: "input",
+        message: "How much would you like to bid?",
+        name: "bid"
+    }])
+    console.log("You have successfully bid on this item!");
+    return submittedBid;
+}
+
+async function mainApp() {
+    let startup = await initialPrompt()
+    console.log(startup.action)
+    if (startup.action == "Post an item") {
+        let post = await postItem()
+        console.log(post.post);
+        console.log(post.description); 
+    } else {
+        console.log("You have chosen BID.")
+        let bid = await bidItem()
+        console.log(bid.items);
+        console.log(bid.bid);
+    }
+}
+
+mainApp();
+
+
+
+
+
+
+    
